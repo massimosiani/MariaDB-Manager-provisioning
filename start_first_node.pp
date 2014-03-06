1053b37b4769bdd62a5c49bd27ad256a6d405bd6
Massimo Siani <massimo.siani@skysql.com>
2014-03-06 17:36:26 +0000

    Fix copyright

--- start_first_node.pp
+++ start_first_node.pp
@@ -12,7 +12,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 51
 # Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-# Copyright 2012-2014 SkySQL Ab
+# Copyright 2014 SkySQL Ab
 #
 # Author: Massimo Siani
 # Date: January 2014
